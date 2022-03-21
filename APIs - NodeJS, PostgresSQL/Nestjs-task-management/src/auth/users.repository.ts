cd6b4d31c8f48509e040b08016a5afd74c25509f
Guilherme Moreira <guilhermi_silva@hotmail.com>
2022-03-21 14:28:22 -0300

    add criptografia de senha

--- APIs - NodeJS, PostgresSQL/Nestjs-task-management/src/auth/users.repository.ts
+++ APIs - NodeJS, PostgresSQL/Nestjs-task-management/src/auth/users.repository.ts
@@ -2,6 +2,7 @@ import { ConflictException, InternalServerErrorException } from "@nestjs/common"
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/AuthCredentials.dto";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcrypt'
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -29,9 +30,12 @@ export class UsersRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(password, salt)
+
         const user = this.create({
             username,
-            password,
+            password: hashedPassword,
         });
 
         try {
