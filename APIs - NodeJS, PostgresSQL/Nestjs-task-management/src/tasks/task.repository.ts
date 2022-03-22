dd1e5e6bdb26812ffc58e7883729a5462014e810
Guilherme Moreira <guilhermi_silva@hotmail.com>
2022-03-22 11:47:55 -0300

    add logs

--- APIs - NodeJS, PostgresSQL/Nestjs-task-management/src/tasks/task.repository.ts
+++ APIs - NodeJS, PostgresSQL/Nestjs-task-management/src/tasks/task.repository.ts
@@ -4,9 +4,13 @@ import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 import { TaskStatus } from "./task-status.enum";
 import { Task } from "./task.entity";
+import { InternalServerErrorException, Logger } from '@nestjs/common';
+import { timestamp } from "rxjs";
+
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
+    private logger = new Logger('TasksRepository', { timestamp: true })
 
     async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
         const { status, search } = filterDto
@@ -24,9 +28,17 @@ export class TasksRepository extends Repository<Task> {
             )
         }
 
-        const tasks = await query.getMany();
+        try {
+            const tasks = await query.getMany();
+            return tasks
 
-        return tasks
+        } catch (error) {
+            this.logger.error(
+                `Failed to get tasks for user "${user.username}". Filters: ${JSON.stringify(filterDto)}`,
+                error.stack
+            )
+            throw new InternalServerErrorException()
+        }
     }
 
     async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
--- APIs - NodeJS, PostgresSQL/Nestjs-task-management/src/tasks/tasks.controller.ts
+++ APIs - NodeJS, PostgresSQL/Nestjs-task-management/src/tasks/tasks.controller.ts
@@ -7,10 +7,11 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpadteTaskStatusDto } from './dto/update-task.dto';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
-
+import { Logger } from '@nestjs/common';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+    private logger = new Logger('TasksController')
     constructor(private tasksServices: TasksService) { }
 
     @Get()
@@ -18,6 +19,7 @@ export class TasksController {
         @Query() filterDto: GetTasksFilterDto,
         @GetUser() user: User
     ): Promise<Task[]> {
+        this.logger.verbose(`User "${user.username}" retrieving all tasks. Filters ${JSON.stringify(filterDto)}`)
         return this.tasksServices.getTasks(filterDto, user)
     }
 
@@ -34,6 +36,7 @@ export class TasksController {
         @Body() createTaskDto: CreateTaskDto,
         @GetUser() user: User
     ): Promise<Task> {
+        this.logger.verbose(`User "${user.username}" create a new task. Data: ${createTaskDto}`)
         return this.tasksServices.createTask(createTaskDto, user)
     }
 
